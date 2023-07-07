b37c1f26e51649da973632ad05aa85a581161100
Alberto Mulone <alberto.mulone@edu.unito.it>
2023-07-07 14:09:40 +0200

    changed token availability methods

--- streamflow/workflow/token.py
+++ streamflow/workflow/token.py
@@ -91,7 +91,7 @@ class ListToken(Token):
 
     async def is_available(self, context: StreamFlowContext):
         return all(
-            asyncio.gather(
+            await asyncio.gather(
                 *(asyncio.create_task(t.is_available(context)) for t in self.value)
             )
         )
@@ -143,7 +143,7 @@ class ObjectToken(Token):
 
     async def is_available(self, context: StreamFlowContext):
         return all(
-            asyncio.gather(
+            await asyncio.gather(
                 *(
                     asyncio.create_task(t.is_available(context))
                     for t in self.value.values()
